32b228a189259ec18e7905cc6f132ec38c09aeea
Carolyn Vasisko <carolyn@mothermedia.com>
2018-05-06 17:00:15 -0500

    password removed

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection= mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Mysqlr00t",
+    password: "",
     database: "burgers_db"
   });
 
